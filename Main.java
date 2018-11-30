a8267ffcde16a9dbb1631135f835d69dad5bd4b3
Diego Fratta <dfratta@wisc.edu>
2018-11-30 17:13:34 -0600

    updating the javaFX

--- Main.java
+++ Main.java
@@ -1,26 +1,62 @@
 package application;
 
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class Main extends Application{
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static void main(String[] args) {
 		launch(args);
+
+	}
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hello World!");
+		StackPane root = new StackPane();
+		StackPane second = new StackPane();
+		Scene scene1 = new Scene(root, 500, 500);
+		VBox VB1 = new VBox();
+		VBox VB2 = new VBox();
+		HBox HB = new HBox();
+		Button analyzeMeal = new Button();
+		Label fileInput = new Label();
+		TextField fileInputField = new TextField();
+		fileInput.setText("Enter your file name");
+		analyzeMeal.setText("Click to analyze your meal");
+		analyzeMeal.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Meal analysis");
+			}
+		});
+		fileInputField.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//Placeholder message until back end functionality is implemented
+				System.out.println("This is when the file would be read");
+			}
+		});
+		ListView<String> list = new ListView<String>();
+		ObservableList<String> items =FXCollections.observableArrayList (
+		    "Beans", "Burgers", "Fries", "Nuggets");
+		list.setItems(items);
+		VB1.getChildren().addAll(analyzeMeal, list);
+		VB2.getChildren().addAll(fileInput, fileInputField);
+		HB.getChildren().addAll(VB1, VB2);
+	root.getChildren().add(HB);
+	primaryStage.setScene(scene1);
+	primaryStage.show();
 	}
 }
