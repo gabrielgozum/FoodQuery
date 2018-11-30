5676727cf8f16fb5f8cd1073cc50fec379faaf8b
Diego Fratta <dfratta@wisc.edu>
2018-11-30 17:30:26 -0600

    diego's edits

--- Main.java
+++ Main.java
@@ -11,9 +11,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -23,12 +25,16 @@ public class Main extends Application{
 
 	}
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hello World!");
+		BorderPane borderPanel = new BorderPane();
+		Label topLabel = new Label("     FoodQuery and Meal Analysis");
+		topLabel.setFont(new Font("Arial", 30));
+		borderPanel.setTop(topLabel);
+		primaryStage.setTitle("FoodQuery and Meal Analysis");
 		StackPane root = new StackPane();
-		StackPane second = new StackPane();
 		Scene scene1 = new Scene(root, 500, 500);
 		VBox VB1 = new VBox();
 		VBox VB2 = new VBox();
+		VBox VB3 = new VBox();
 		HBox HB = new HBox();
 		Button analyzeMeal = new Button();
 		Label fileInput = new Label();
@@ -50,12 +56,17 @@ public class Main extends Application{
 		});
 		ListView<String> list = new ListView<String>();
 		ObservableList<String> items =FXCollections.observableArrayList (
-		    "Beans", "Burgers", "Fries", "Nuggets");
+		   "Apples", "Avacado", "Bananas", "Beans", "Burgers", "Fries", "Granola Bar", "Grapefruit", "Nuggets", "Pizza");
 		list.setItems(items);
+		list.setPrefWidth(100);
+		list.setPrefHeight(70);
 		VB1.getChildren().addAll(analyzeMeal, list);
-		VB2.getChildren().addAll(fileInput, fileInputField);
-		HB.getChildren().addAll(VB1, VB2);
-	root.getChildren().add(HB);
+		VB1.setSpacing(10);
+		VB3.getChildren().addAll(fileInput, fileInputField);
+		HB.getChildren().addAll(VB1, VB2, VB3);
+	//root.getChildren().add(HB);
+	borderPanel.setLeft(VB1);
+	root.getChildren().add(borderPanel);
 	primaryStage.setScene(scene1);
 	primaryStage.show();
 	}
