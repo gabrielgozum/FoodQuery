22a79c726ef2e127fc2f46f7975d0ed018e144bd
Diego Fratta <dfratta@wisc.edu>
2018-11-30 18:15:54 -0600

    fuck main

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -26,19 +27,56 @@ public class Main extends Application{
 	}
 	public void start(Stage primaryStage) {
 		BorderPane borderPanel = new BorderPane();
-		Label topLabel = new Label("     FoodQuery and Meal Analysis");
-		topLabel.setFont(new Font("Arial", 30));
-		borderPanel.setTop(topLabel);
+
 		primaryStage.setTitle("FoodQuery and Meal Analysis");
 		StackPane root = new StackPane();
-		Scene scene1 = new Scene(root, 500, 500);
-		VBox VB1 = new VBox();
-		VBox VB2 = new VBox();
-		VBox VB3 = new VBox();
-		HBox HB = new HBox();
+		Scene scene1 = new Scene(root, 600, 600);
+		VBox VB1 = new VBox(); 	//Used in left Border
+		VBox VB2 = new VBox();	//Used in right border
+		VBox VB3 = new VBox();	//Used in bottom border in HB
+		VBox VB4 = new VBox();	//Used in the center
+		VBox VB5 = new VBox(); 	//Used in bottom border in HB
+		VBox VB6 = new VBox(); 	//Used in bottom border in HB
+		HBox HB = new HBox();	//Used in bottom border
+		HBox nameBox = new HBox();
+		HBox proteinBox = new HBox();
+		HBox fiberBox = new HBox();
+		HBox calorieBox = new HBox();
+		HBox carbohydrateBox = new HBox();
+		HBox fatBox = new HBox();
+		
 		Button analyzeMeal = new Button();
+		
+		Label topLabel = new Label("FoodQuery and Meal Analysis");
+		topLabel.setFont(new Font("Arial", 30));
+		Label individualFood = new Label("Add Food Manually: ");
 		Label fileInput = new Label();
+		Label foodName = new Label("Name: ");
+		Label foodProtein = new Label("Protein: ");
+		Label foodFiber = new Label("Fiber: ");
+		Label foodCalories = new Label("Calories :");
+		Label foodCarbs = new Label("Carbohydrates:");
+		Label foodFat = new Label("Fat: ");
+		Label foodListLabel = new Label("Food List");
+		Label mealListLabel = new Label("Meal List");
+		
+		TextField foodNameField = new TextField();
+		TextField foodProteinField = new TextField();
+		TextField foodFiberField = new TextField();
+		TextField foodCaloriesField = new TextField();
+		TextField foodCarbsField = new TextField();
+		TextField foodFatField = new TextField();
 		TextField fileInputField = new TextField();
+		TextField foodListSearchBar = new TextField("Search");
+		TextField mealListSearchBar = new TextField("Search");
+		
+		nameBox.getChildren().addAll(foodName, foodNameField);
+		proteinBox.getChildren().addAll(foodProtein, foodProteinField);
+		fiberBox.getChildren().addAll(foodFiber, foodFiberField);
+		calorieBox.getChildren().addAll(foodCalories, foodCaloriesField);
+		carbohydrateBox.getChildren().addAll(foodCarbs, foodCarbsField);
+		fatBox.getChildren().addAll(foodFat, foodFatField);
+		
 		fileInput.setText("Enter your file name");
 		analyzeMeal.setText("Click to analyze your meal");
 		analyzeMeal.setOnAction(new EventHandler<ActionEvent>() {
@@ -56,23 +94,36 @@ public class Main extends Application{
 		});
 		ListView<String> list = new ListView<String>();
 		ObservableList<String> items =FXCollections.observableArrayList (
-		   "Apples", "Avacado", "Bananas", "Beans", "Burgers", "Fries", "Granola Bar", "Grapefruit", "Nuggets", "Pizza");
+		   "Apples", "Avacado", "Bananas", "Beans", "Beer", "Burgers", "Fries", "Granola Bar", "Grapefruit", "Nuggets", "Pizza");
 		list.setItems(items);
 		list.setPrefWidth(100);
-		list.setPrefHeight(70);
-		VB1.getChildren().addAll(analyzeMeal, list);
-		VB1.setSpacing(10);
-	//root.getChildren().add(HB);
+		list.setPrefHeight(250);
 
 		ListView<String> mealList = new ListView<String>();
 		ObservableList<String> mealItems = FXCollections.observableArrayList(
 				"Burgers", "Fries", "Beer", "Fruit");
 		mealList.setItems(mealItems);
-		VB2.getChildren().addAll(fileInput, fileInputField, mealList);
-		HB.getChildren().addAll(VB1, VB2, VB3);
+		mealList.setPrefWidth(100);
+		mealList.setPrefHeight(150);
+		
+		VB1.getChildren().addAll(foodListLabel, foodListSearchBar, list);
+		VB1.setSpacing(10);
+		VB2.getChildren().addAll(mealListLabel, mealListSearchBar, mealList, analyzeMeal);
+		VB2.setSpacing(10);
+		VB3.getChildren().addAll(fileInput, fileInputField); 
+
+		VB5.getChildren().addAll(individualFood, nameBox, proteinBox, fiberBox);
+		VB6.getChildren().addAll(calorieBox, carbohydrateBox, fatBox);
+		HB.getChildren().addAll(VB3, VB5, VB6);
+		
+		
+		borderPanel.setTop(topLabel);
 		borderPanel.setLeft(VB1);
 		borderPanel.setRight(VB2);
+		borderPanel.setBottom(HB);
+		borderPanel.setCenter(VB4);
 		root.getChildren().add(borderPanel);
+		
 	primaryStage.setScene(scene1);
 	primaryStage.show();
 	}
