b7f405ccef1067b7b5b051f9966a6cd55caafb43
Gabriel <ggozum@wisc.edu>
2018-12-04 16:06:25 -0600

    added some functionality to the fooddata class

--- FoodData.java
+++ FoodData.java
@@ -45,18 +45,17 @@ public class FoodData implements FoodDataADT<FoodItem> {
     	{
 			File file = new File(filePath);
 			Scanner input = new Scanner(file);
-			FoodItem cur = null;
-			boolean skipLine = false;
+			FoodItem cur = null; //FoodItem to be added to List
+			boolean skipLine = false; // used for incorrect format
 			
 			while(input.hasNextLine()) {
 				String line = input.nextLine();
 				try 
 				{
-					String[] data = line.split(",", 12);
-					skipLine = false;
-	
+					String[] data = line.split(",", 12); //split csv line into separate strings
+					skipLine = false; // initialize skipLine to false
+
 					cur = new FoodItem(data[0], data[1]); // adds food item with id and name
-					
 					
 					for(int i = 2; i < data.length; i = i + 2)
 					{
@@ -105,8 +104,53 @@ public class FoodData implements FoodDataADT<FoodItem> {
      */
     @Override
     public List<FoodItem> filterByNutrients(List<String> rules) {
-        // TODO : Complete
-        return null;
+    	  /**
+         * Gets all the food items that fulfill ALL the provided rules
+         *
+         * Format of a rule:
+         *     "<nutrient> <comparator> <value>"
+         * 
+         * Definition of a rule:
+         *     A rule is a string which has three parts separated by a space:
+         *         1. <nutrient>: Name of one of the 5 nutrients [CASE-INSENSITIVE]
+         *         2. <comparator>: One of the following comparison operators: <=, >=, ==
+         *         3. <value>: a double value
+         * 
+         * Note:
+         *     1. Multiple rules can contain the same nutrient.
+         *         E.g. ["calories >= 50.0", "calories <= 200.0", "fiber == 2.5"]
+         *     2. A FoodItemADT object MUST satisfy ALL the provided rules i
+         *        to be returned in the filtered list.
+         *
+         * @param rules list of rules
+         * @return list of filtered food items; if no food item matched, return empty list
+         */
+    	ArrayList<FoodItem> filtered = new ArrayList<FoodItem>();
+    	String[] ruleArray;
+    	for(String r : rules)
+    	{
+    		ruleArray = r.split(" ");
+    		ruleArray[0] = ruleArray[0].toLowerCase(); // case insensitive for nutrient
+    		switch(ruleArray[1])
+    		{
+    			case ">=":
+    			{
+    				// TODO: >=
+    			}
+    			case "<=":
+    			{
+    				// TODO <=
+    			}
+    			case "==":
+    			{
+    				
+    			}
+    			default:
+    				// NOTHING
+    		}		
+    	}
+    	
+    	return null;
     }
 
     /*
@@ -130,26 +174,27 @@ public class FoodData implements FoodDataADT<FoodItem> {
 
     @Override
     public void saveFoodItems(String filename) {
-    	//MAY BE IN REVERSE SORTED ORDER
         try
         {
 	    	File file = new File(filename);
 	    	BufferedWriter out = new BufferedWriter(new FileWriter(file));
 	    	
-	    	Comparator<FoodItem> foodComparator = (a, b) -> a.getName().compareTo(b.getName());
-	    	ArrayList<FoodItem> sortedNames = (ArrayList<FoodItem>) foodItemList.stream().sorted(foodComparator).collect(Collectors.toList());
+	    	Comparator<FoodItem> foodComparator = (a, b) -> a.getName().compareTo(b.getName());  // Sort by name comparator
+	    	ArrayList<FoodItem> sortedNames = (ArrayList<FoodItem>) foodItemList.stream().sorted(foodComparator).collect(Collectors.toList()); //ArrayList of sorted names
 	    	
-	    	String line;
+	    	String line; //line to be appended to saved file
 	        for(FoodItem i : sortedNames)
 	        {
-	        	line = i.getID() + "," + i.getName();
-	        	HashMap<String, Double> nutrients = i.getNutrients();
+	        	line = i.getID() + "," + i.getName(); //add ID and name to line
+	        	
+	        	HashMap<String, Double> nutrients = i.getNutrients(); //add nutrients to line
 	        	for(String j : nutrients.keySet())
 	        	{
 	        		line = line + "," + j + "," + Double.toString(nutrients.get(j));
 	        	}
+	        	
 	        	out.write(line);
-	        	out.newLine(); //possibly unnecessary
+	        	out.newLine();
 	        }
 	        out.close();
         } catch (IOException e) {
